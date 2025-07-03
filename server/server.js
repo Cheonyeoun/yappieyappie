9ee951fb96f02d2c2782f3d558391a99e148c5e9
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-07-03 16:37:56 +0530

    Add Socket.IO setup to backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
+const http = require('http');
+const { Server } = require('socket.io');
+ 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -15,11 +18,33 @@ mongoose.connect(MONGO_URI)
 .catch((err)=> console.error("❌ Failed!",err) )
 
 
+const io = new Server(server,{
+    cors:{
+        origin:'*',
+        methods:['GET','POST']
+    }
+})
+
+
+// Socket handling
+io.on('connection',(socket)=>{
+    console.log("✔️ User Connected",socket.id);
+    socket.on('send-message',(data)=>{
+        console.log("message-received",data);
+        io.emit('received-message',data);
+    })
+    socket.on('disconnect',()=>{
+        console.log("❌ User Disconnected",socket.id);
+    })
+})
+
+
+
 app.get('/',(req,res)=>{
     return res.send({message:"Welcome To Server!"});
 })
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
